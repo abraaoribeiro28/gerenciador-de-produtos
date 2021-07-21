a23f4c21194ec0f4c6ce1d8f691d3fbe2c95ae5b
Abraão Ribeiro <abraaobeiro@gmail.com>
2021-07-21 15:37:13 -0300

    Criei uma nova view pro layouts
    View com um modal para confirmar se deseja excluir algo, é exibido em mais de uma tela, então para evitar códigos repetidos, basta incluir este arquivo onde o código se repete

---
+++ resources/views/layouts/modal-delete.blade.php
@@ -0,0 +1,23 @@
+<div class="modal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Confirmar exclusão</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="fecharModal()">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Tem certeza que deseja excluir este produto?</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" onclick="fecharModal()">Cancelar</button>
+          <form method="post" id="form-excluir">
+            @csrf
+            @method('delete')
+            <button type="submit" class="btn btn-primary">Confirmar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+</div>
