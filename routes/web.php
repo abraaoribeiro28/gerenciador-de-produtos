e85f8823bc4411943a1105dc12f6a7a1226e21be
Abraão Ribeiro <abraaobeiro@gmail.com>
2021-07-23 10:57:29 -0300

    Modificação nas rotas
    Agrupei as rotas

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,30 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 // Products
-Route::get('/products', [ProductController::class, 'index'])->name('products')->middleware('auth');
-Route::get('/products/category/{id}', [ProductController::class, 'index'])->name('products.filter')->middleware('auth');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create')->middleware('auth');
-Route::post('/products/create', [ProductController::class, 'store'])->name('products.store')->middleware('auth');
-Route::get('/product/edit/{id}', [ProductController::class, 'edit'])->name('product.edit')->middleware('auth');
-Route::put('/product/edit/{id}', [ProductController::class, 'update'])->name('product.update')->middleware('auth');
-Route::delete('/product/delete/{id}', [ProductController::class, 'delete'])->name('product.delete')->middleware('auth');
+Route::prefix('products')->group(function(){
+    Route::get('/', [ProductController::class, 'index'])->name('products')->middleware('auth');
+    Route::get('category/{id}', [ProductController::class, 'index'])->name('products.filter')->middleware('auth');
+    Route::get('create', [ProductController::class, 'create'])->name('products.create')->middleware('auth');
+    Route::post('create', [ProductController::class, 'store'])->name('products.store')->middleware('auth');
+});
+Route::prefix('product')->group(function(){
+    Route::get('edit/{id}', [ProductController::class, 'edit'])->name('product.edit')->middleware('auth');
+    Route::put('edit/{id}', [ProductController::class, 'update'])->name('product.update')->middleware('auth');
+    Route::delete('product/delete/{id}', [ProductController::class, 'delete'])->name('product.delete')->middleware('auth');
+});
+
 
 // Categories
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories')->middleware('auth');
-Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create')->middleware('auth');
-Route::post('/category/create', [CategoryController::class, 'store'])->name('category.store');
-Route::get('/category/{id}', [CategoryController::class, 'show'])->name('category.show');
-Route::delete('/category/delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy')->middleware('auth');
-Route::get('/category/edit/{id}', [CategoryController::class, 'edit'])->name('category.edit')->middleware('auth');
-Route::put('/category/edit/{id}', [CategoryController::class, 'update'])->name('category.update')->middleware('auth');
+Route::prefix('category')->group(function(){
+    Route::get('create', [CategoryController::class, 'create'])->name('category.create')->middleware('auth');
+    Route::post('create', [CategoryController::class, 'store'])->name('category.store');
+    Route::get('{id}', [CategoryController::class, 'show'])->name('category.show');
+    Route::delete('delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy')->middleware('auth');
+    Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('category.edit')->middleware('auth');
+    Route::put('edit/{id}', [CategoryController::class, 'update'])->name('category.update')->middleware('auth');
+});
+
 
 // Archives
 Route::get('/archives', [ArchiveController::class, 'index'])->name('archives')->middleware('auth');
