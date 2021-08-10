88dd8020a43b11f9dba5c74624b4e6beda5a6114
Abraão Ribeiro <abraaobeiro@gmail.com>
2021-08-10 12:54:53 -0300

    Create show.blade.php

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.index')
+
+@section('title', 'Gerenciador - Produto')
+
+@section('content')
+<section class="section-products" id="section">
+  <div class="container px-5">
+    
+  </div>
+</section>
+
+@include('layouts.modal-delete')
+
+@endsection
