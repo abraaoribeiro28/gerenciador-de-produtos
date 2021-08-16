fe6cac91494428e6406aa3e8deba725642f25d40
Abraão Ribeiro <abraaobeiro@gmail.com>
2021-08-16 16:44:06 -0300

    Update home.blade.php

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -6,7 +6,7 @@
 <section class="landing">
     <div class="container px-5">
         <div class="row">
-            <div class="col-6">
+            <div class="col-md-6 col-12">
                 <div class="d-flex justify-content-center align-items-center h-100">
                     <div>
                         <h2 class="main_title">Gerenciamento de Produtos</h2>
@@ -14,7 +14,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-md-6 col-8 offset-2 offset-md-0">
                 <img class="img-fluid" src="/images/home/gerenciamento.svg" alt="">
             </div>
         </div>
@@ -22,10 +22,10 @@
 </section>
 <section class="design-responsivo container">
     <div class="row">
-        <div class="col-6">
+        <div class="col-md-6 col-8 offset-2 offset-md-0">
             <img  src="/images/home/designer-animate.svg" class="img-fluid" id="imagem-resposive">
         </div>
-        <div class="col-6 d-flex align-items-center">
+        <div class="col-md-6 col-12 d-flex align-items-center">
             <div>
                 <h2 class="main_title">Design responsivo</h2>
                 <p class="main_description">
