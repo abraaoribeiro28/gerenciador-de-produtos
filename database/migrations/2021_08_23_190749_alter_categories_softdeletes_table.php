dd4ab07c22eaf63470c0158b6ab681cd4a10ba30
Abraão Ribeiro <abraaobeiro@gmail.com>
2021-08-23 16:19:21 -0300

    Implementando SoftDeletes

--- database/migrations/2021_08_23_190749_alter_categories_softdeletes_table.php
+++ database/migrations/2021_08_23_190749_alter_categories_softdeletes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterProductsSoftdeletesTable extends Migration
+class AlterCategoriesSoftdeletesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AlterProductsSoftdeletesTable extends Migration
      */
     public function up()
     {
-        Schema::table('products', function (Blueprint $table) {
+        Schema::table('categories', function(Blueprint $table){
             $table->softDeletes();
         });
     }
@@ -25,7 +25,7 @@ class AlterProductsSoftdeletesTable extends Migration
      */
     public function down()
     {
-        Schema::table('products', function ($table) {
+        Schema::table('categories', function ($table) {
             $table->dropSoftDeletes();
         });
     }
