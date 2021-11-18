b3052108f9052f78bd75139f6e613af265a9ad63
Abraão Ribeiro <abraaobeiro@gmail.com>
2021-11-18 11:51:31 -0300

    Create ArchiveController.php
    Cria o ArchiveController e implementa os métodos para Api

---
+++ app/Http/Controllers/Api/ArchiveController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Archive;
+use App\Models\Product;
+
+
+class ArchiveController extends Controller
+{
+    private $archive;
+
+    public function __construct(Archive $archive){
+        $this->archive = $archive;
+    }
+
+    public function index()
+    {
+        $archives = $this->archive->all();
+
+        return response()->json($archives);
+    }
+
+
+    public function store(Request $request)
+    {
+        $archive = $this->archive;
+        if($request->hasFile('archive') && $request->file('archive')->isValid()){
+            $name = $request->archive->getClientOriginalName();
+            $upload = $request->archive->storeAs('products', $name);
+            $archive->archive =  $name;
+            if(!$upload){
+                return response()->json(['error' => 'Fail_upload'], 500);
+            }
+        }
+        $archive->save();
+
+        return response()->json($archive, 201);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::where('archive_id', $id)->get();
+        if(count($product) > 0){
+            return response()->json(['error' => 'Have products using the image']);
+        }
+
+        $archive = $this->archive->find($id);
+        if(!$archive){
+            return response()->json(['error' => 'Not found'], 404);
+        }
+        $archive->delete();
+        return response()->json([], 204);
+    }
+}
