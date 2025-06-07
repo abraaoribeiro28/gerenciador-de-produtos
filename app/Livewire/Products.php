2b19f5375f3c7f558a3d9ad1bb39e6aeb767fe8c
Abraão Ribeiro <abraaobeiro@gmail.com>
2025-06-07 13:43:47 -0300

    feat(products): add filter, search, and sorting to products table

--- app/Livewire/Products.php
+++ app/Livewire/Products.php
@@ -2,12 +2,40 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class Products extends Component
 {
+    // Search and sorting
+    public ?string $search = null;
+    public ?string $sortBy = null;
+    public ?string $sortDir = null;
+
     public function render()
     {
-        return view('livewire.products');
+        $products = Product::queryWithFilters(
+            $this->sortBy,
+            $this->sortDir,
+            $this->search
+        )->paginate();
+        return view('livewire.products', compact('products'));
+    }
+
+
+    /**
+     * Handle table sorting logic.
+     * Toggles the sort direction if the same column is clicked again.
+     *
+     * @param string $column
+     * @return void
+     */
+    public function sort(string $column): void
+    {
+        $this->sortDir = ($this->sortBy === $column && $this->sortDir === 'asc')
+            ? 'desc'
+            : 'asc';
+
+        $this->sortBy = $column;
     }
 }
